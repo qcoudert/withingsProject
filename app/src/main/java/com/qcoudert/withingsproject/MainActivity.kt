52c39f52cb19a621067321256c416142efb03db2
qcoudert <quentin.coudert1@gmail.com>
2021-08-14 13:25:16 +0200

    Init Commit

---
+++ app/src/main/java/com/qcoudert/withingsproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.qcoudert.withingsproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qcoudert.withingsproject.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/qcoudert/withingsproject/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.qcoudert.withingsproject.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
